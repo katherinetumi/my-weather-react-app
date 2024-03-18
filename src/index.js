bf03d9b3ed9926b42a5f624c75da2cc1f8288797
itumelengkolwane <162047181+katherinetumi@users.noreply.github.com>
2024-03-18 13:17:10 +0200

    update indesx.js , index.css, app.js ,app.css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './App.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div ClassName="weather">
+      <h1>Weather app</h1>
     <App />
+    </div>
   </React.StrictMode>
 );
 
