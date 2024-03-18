148ee29f5cdf967dd8c1c1d6910359a818ded6b1
itumelengkolwane <162047181+katherinetumi@users.noreply.github.com>
2024-03-18 13:27:27 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function WeatherSearch() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiKey = "3c949ba49d38be2487ee278e0d2d4059";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
